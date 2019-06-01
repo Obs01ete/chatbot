c1ac03f064474d46b2837d79eda46435884980b4
Dmitrii Khizbullin <dmitrii.khizbullin@gmail.com>
2019-06-01 13:01:45 +0200

    Code cleanup

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,6 @@
+import sys
 import json
+import time
 import emoji
 import argparse
 import http.client
@@ -46,7 +48,7 @@ def application(request):
         if request.path == "/send" and request.content_type == "application/json":
             response_str = "Message received and being processed"
             message_dict = json.loads(request.data)
-            print(message_dict)
+            print("Message from client:", message_dict)
 
             user = message_dict['user']
             message = message_dict['message']
@@ -54,9 +56,9 @@ def application(request):
             g_message_history.append_message(message_dict['user'], message)
 
             bot_reply = g_sentiment(message)
-            emoji_str = ":happy:" if bot_reply else ":sad:"
-            bot_emoji = emoji.emojize(emoji_str)
-            bot_reply_full = f"[BOT] {bot_emoji}"
+            emoji_str = ":smiley:" if bot_reply else ":worried:"
+            bot_emoji = emoji.emojize(emoji_str, use_aliases=True)
+            bot_reply_full = f"[BOT] {bot_emoji} ({emoji_str})"
             g_message_history.append_message(user, bot_reply_full)
         else:
             response_str = "Unknown POST format"
@@ -77,7 +79,7 @@ def application(request):
         response_str = ""
         print("Have no idea what to do with this request")
 
-    print(response_str)
+    print("Responding:", response_str)
     return Response(response_str)
 
 
@@ -90,6 +92,7 @@ def launch_server():
 class BotClient:
     def __init__(self, user_name):
         print("Launching client")
+        print(f"Logging in as {user_name}")
         self._user_name = user_name
         self._conn = http.client.HTTPConnection(CLIENT_URL + ":" + str(PORT))
         self._last_seen = None
@@ -107,13 +110,11 @@ class BotClient:
             }
             self._conn.request("GET", "/messages", params, headers)
             response = self._conn.getresponse()
-            #print(response.status, response.reason)
             json_str = response.read()
             messages_dict = json.loads(json_str)
-            print(messages_dict)
             if 'messages' in messages_dict and 'last_seen' in messages_dict:
                 for message in messages_dict['messages']:
-                    print("Bot:", message)
+                    print("Chat client:", message)
                 self._last_seen = messages_dict['last_seen']
             else:
                 pass
@@ -130,9 +131,7 @@ class BotClient:
         try:
             self._conn.request("POST", "/send", content, headers)
             response = self._conn.getresponse()
-            #print(response.status, response.reason)
             data = response.read()
-            print(data)
         except:
             print("Cannot connect 2")
 
@@ -141,16 +140,29 @@ class BotClient:
 
 
 def launch_client():
-    import time
-    time.sleep(0.5)
+    time.sleep(0.1)
+
     bc = BotClient("Dmitrii")
+
     bc.fetch_messages()
-    bc.send_message("I am so happy!")
+    bc.send_message("I'm very happy that my team won the world cup!")
     bc.fetch_messages()
-    bc.send_message("I am sad")
+    bc.send_message("I feel a bit sad today")
     bc.fetch_messages()
-    bc.send_message("And again happy!")
-    bc.fetch_messages()
+
+    try:
+        while True:
+            sys.stdout.write(">> ")
+            sys.stdout.flush()
+            line = sys.stdin.readline()
+            stripped_line = line.rstrip()
+            if stripped_line == "":
+                continue
+            bc.send_message(stripped_line)
+            bc.fetch_messages()
+    except KeyboardInterrupt:
+        print("Bye!")
+
     pass
 
 
